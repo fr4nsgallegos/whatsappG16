3dac61963b336375127aa98c9f3cf5c9c7416731
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2026-08-27 21:01:36 -0500

    tabbar whatsapp

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,8 @@ import 'package:whatsappg16/examples/tabbar_dinamico_page.dart';
 import 'package:whatsappg16/examples/tabbar_manual_page.dart';
 import 'package:whatsappg16/examples/tabbar_page.dart';
 import 'package:whatsappg16/pages/home_page.dart';
+import 'package:whatsappg16/pages/whatsapp_home.dart';
 
 void main() {
-  runApp(
-    MaterialApp(home: TabbarDinamicoPage(), debugShowCheckedModeBanner: false),
-  );
+  runApp(MaterialApp(home: WhatsappHome(), debugShowCheckedModeBanner: false));
 }
--- lib/pages/whatsapp_home.dart
+++ lib/pages/whatsapp_home.dart
@@ -1,10 +1,61 @@
 import 'package:flutter/material.dart';
 
-class WhatsappHome extends StatelessWidget {
+class WhatsappHome extends StatefulWidget {
   const WhatsappHome({super.key});
 
   @override
+  State<WhatsappHome> createState() => _WhatsappHomeState();
+}
+
+class _WhatsappHomeState extends State<WhatsappHome>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  int _currentIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+
+    _tabController.addListener(() {
+      if (_currentIndex != _tabController.index) {
+        setState(() {
+          _currentIndex = _tabController.index;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _tabController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xff18816A),
+        foregroundColor: Colors.white,
+        title: Text(
+          "WhatsApp Flutter",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.camera_alt_outlined)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
+        ],
+        bottom: TabBar(
+          controller: _tabController,
+          indicatorColor: Colors.white,
+          labelColor: Colors.white,
+          unselectedLabelColor: Colors.white70,
+          indicatorWeight: 3,
+          tabs: [Text("CHATS"), Text("ESTADOS"), Text("LLAMADAS")],
+        ),
+      ),
+    );
   }
 }
